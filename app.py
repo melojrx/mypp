b1ced6a00a7b511cf237e93fa916eb50cf166049
Melo Jr <jrmeloafrf@gmal.com>
2023-06-14 21:24:10 -0300

    config

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! Teste de Servidor Web em aplicações Flask!! '
+    return 'Hello, World! Versão 2'
 
 if __name__ == '__main__':
     app.run()
